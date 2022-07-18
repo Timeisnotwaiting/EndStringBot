67c9a8132ad11608e17e005866bb778105ecf5d6
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-07-18 22:06:41 +0530

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -1,9 +1,14 @@
-from StringSessionBot.database.users_sql import Users, num_users
+from StringSessionBot.database.users import users
 from StringSessionBot.database import SESSION
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 @Client.on_message(filters.user(1985209910) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
-    users, omfoo = await num_users()
-    await msg.reply(f"👨‍💻 Total Users : \n\n{users}\n{omfoo}", quote=True)
+    ok = await users()
+    m = ""
+    for o in ok:
+        o = str(o)
+        m += f"<code>{o}</code>\n"
+    await msg.reply(f"Users :- \n\n{m}\n\nCount :- {len(ok)}")
+     
