3b7bf37a9386c93f149bebadeb824d3277a5be42
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-07-18 21:57:37 +0530

    Update __init__.py

--- StringSessionBot/database/__init__.py
+++ StringSessionBot/database/__init__.py
@@ -1,15 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from Config import DATABASE_URL
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
+MONGO_DB_URI = "mongodb+srv://keshavalpha:keshavalpha@cluster0.htiys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-def start() -> scoped_session:
-    engine = create_engine(DATABASE_URL)
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-BASE = declarative_base()
-SESSION = start()
+mongo = MongoClient(MONGO_DB_URI)
+db = mongo.SSB
