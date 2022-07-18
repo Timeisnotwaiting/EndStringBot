ee1a6b053aa5a06b690b0fd455bcf8da9ce72481
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-07-18 21:36:13 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -7,9 +7,13 @@ photo = "https://te.legra.ph/file/efecf136bc78da25719fd.jpg"
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, message: Message):
-   user = await bot.get_me()
-   mention = user["mention"]
-   await message.reply_photo(
+    try:
+        add(message.chat.id)
+    except:
+        await bot.send_message(-762062879, f"failed to add {message.chat.id} to database !)
+    user = await bot.get_me()
+    mention = user["mention"]
+    await message.reply_photo(
       photo,
       caption=Data.START.format(message.from_user.mention, mention),
       reply_markup=InlineKeyboardMarkup(Data.buttons))
