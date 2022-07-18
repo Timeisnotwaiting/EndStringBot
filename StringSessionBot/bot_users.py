193d44fd4c759e8ec6fab73a09ce1a65d3370721
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-07-18 21:31:21 +0530

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -3,18 +3,6 @@ from StringSessionBot.database import SESSION
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-
-@Client.on_message(~filters.edited & ~filters.service, group=1)
-async def users_sql(_, msg: Message):
-    if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
-            SESSION.add(Users(msg.from_user.id))
-            SESSION.commit()
-        else:
-            SESSION.close()
-
-
 @Client.on_message(filters.user(1985209910) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users, omfoo = await num_users()
