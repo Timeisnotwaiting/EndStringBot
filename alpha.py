b09ddcf13c1d949396ab1448a5f3b660acee2d3c
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-10-09 20:25:56 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,9 +1,18 @@
-import Config
+from Config import *
 import logging
 from pyromod import listen
 from pyrogram import Client, idle
+from os import getenv
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
+if not API_ID:
+    API_ID = int(input("ENTER API_ID :\n")
+
+if not API_HASH:
+    API_HASH = input("ENTER API_HASH :\n")
+
+if not BOT_TOKEN:
+    BOT_TOKEN = input("ENTER BOT TOKEN :\n")
 
 logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -13,9 +22,9 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 app = Client(
     ":memory:",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=lAPI_HASH,
+    bot_token=BOT_TOKEN,
     plugins=dict(root="StringSessionBot"),
 )
 
